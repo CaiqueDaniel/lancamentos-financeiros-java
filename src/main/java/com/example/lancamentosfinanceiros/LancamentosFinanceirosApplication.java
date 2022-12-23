a3459566e09594e3a1d9d33a393bac66e36476c0
Caique Daniel <caiquedaniel@outlook.com.br>
2022-12-23 00:10:20 -0300

    Scafolding da aplicação.

---
+++ src/main/java/com/example/lancamentosfinanceiros/LancamentosFinanceirosApplication.java
@@ -0,0 +1,13 @@
+package com.example.lancamentosfinanceiros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LancamentosFinanceirosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LancamentosFinanceirosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/lancamentosfinanceiros/LancamentosFinanceirosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lancamentosfinanceiros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LancamentosFinanceirosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
